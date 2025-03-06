062878f52391ba5fee112228dbb29de7df892f1c
人民的勤务员 <china.qinwuyuan@gmail.com>
2025-03-06 13:06:58 +0800

    ci: 🎡 增加仓库提交动态的最大显示数量为`5`

--- src/multi_repo_commit_updater.sh
+++ src/multi_repo_commit_updater.sh
@@ -25,19 +25,35 @@ for GITHUB_REPO in "${REPO_LIST[@]}"; do
     # 提取内容
     EXISTING_LINES=$(awk '/<!-- START gadpp -->/{flag=1;next}/<!-- END gadpp -->/{flag=0}flag' "$README_FILE")
 
+    echo "Check result: $RESULT"
     # 检查行存在
     if echo "$EXISTING_LINES" | grep -q "$COMMIT_SHA"; then
         echo "COMMIT_SHA 已存在"
     else
         awk -v insert="$INSERTED_LINE" '
             BEGIN { inside=0 }
-            /<!-- START gadpp -->/ { print; inside=1; next }
-            /<!-- END gadpp -->/ { if (inside) { print insert }; inside=0 }
+            /<!-- START gadpp -->/ { print; print insert; inside=1; next }
+            /<!-- END gadpp -->/ { inside=0 }
             { print }
         ' "$README_FILE" >readme_changed.md
-
         # 更新完成
         mv readme_changed.md "$README_FILE"
         echo "仓库的最新提交已经被更新到 README 文件中。"
     fi
+
 done
+
+# 最大显示数量
+MAX_COUNT=5
+
+EXISTING_LINES=$(awk '/<!-- START gadpp -->/{flag=1;next}/<!-- END gadpp -->/{flag=0}flag' "$README_FILE" | head -n $MAX_COUNT)
+EXISTING_LINES=$(printf "%s" "$EXISTING_LINES")
+sed -i "/<!-- START gadpp -->/,/<!-- END gadpp -->/{
+    /<!-- START gadpp -->/{
+        n
+        r /dev/stdin
+        d
+    }
+    /<!-- END gadpp -->/!d
+}" "$README_FILE" <<<"$EXISTING_LINES"
+echo "$EXISTING_LINES"
